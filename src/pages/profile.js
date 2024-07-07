66dd3c89e8a5ffd231cfb943e1a4761aa7001af6
William <williamjoel273@gmail.com>
2024-07-06 21:14:14 -0500

    Portafolio terminado, se deben agregar las fotos en la carpeta profile

---
+++ src/pages/profile.js
@@ -0,0 +1,114 @@
+export const skills = [
+  {
+    skill: "Javascript",
+    percentage: 100,
+  },
+  {
+    skill: "TypeScript",
+    percentage: 100,
+  },
+  {
+    skill: "Nodejs",
+    percentage: 100,
+  },
+  {
+    skill: "Next",
+    percentage: 100,
+  },
+  {
+    skill: "MongoDB",
+    percentage: 100,
+  },
+  {
+    skill: "React",
+    percentage: 100,
+  },
+  {
+    skill: "Bootstrap",
+    percentage: 100,
+  },
+];
+
+export const experiences = [
+  {
+    title: "Software developer at Microsoft",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem officiis fugiat vel animi aliquam inventore rem. Quo laudantium temporibus cupiditate. Aut?",
+    from: 2000,
+    to: 2005,
+  },
+  {
+    title: "Software developer at Google",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem officiis fugiat vel animi aliquam inventore rem. Quo laudantium temporibus cupiditate. Aut?",
+    from: 2005,
+    to: 2007,
+  },
+  {
+    title: "Software developer at SpaceX",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem officiis fugiat vel animi aliquam inventore rem. Quo laudantium temporibus cupiditate. Aut?",
+    from: 2010,
+  },
+];
+
+export const projects = [
+  {
+    name: "Awesome Website 1",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "https://st1.uvnimg.com/dims4/default/e3a32fa/2147483647/thumbnail/480x270/quality/75/?url=http%3A%2F%2Fuvn-brightspot.s3.amazonaws.com%2F7b%2F2c%2F6d350d9e42fab0c8addd512a3a66%2Fgoku-dragon-ball-super-poder.jpg",
+  },
+  {
+    name: "Awesome Website 2",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "https://i.pinimg.com/736x/78/33/a4/7833a421a36aebab7c635e47462d0ced.jpg",
+  },
+  {
+    name: "Awesome Website 3",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "https://img.asmedia.epimg.net/resizer/v2/XUK5LKJW7RE7ZFG7DQ5NJ5VRZY.jpg?auth=f3a40ffd18bebb8bdb0f25ada28b0a223efcaf004300a8933269fd9934d72027&width=360&height=203&smart=true",
+  },
+  {
+    name: "Awesome Website 4",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "https://sm.ign.com/t/ign_latam/screenshot/default/goku-ui_a17m.1280.jpg",
+  },
+  {
+    name: "Awesome Website 5",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "https://areajugones.sport.es/wp-content/uploads/2022/11/goku-ui-verdadero.jpg",
+  },
+  {
+    name: "Awesome Website 6",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    image: "https://i.blogs.es/23bb31/apps.16480.13656678855925393.22ab9b41-5579-449c-81e5-ff46ea0e30b6/650_1200.webp",
+  },
+];
+
+export const posts = [
+  {
+    title: "React",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Angular",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+  {
+    title: "Nextjs",
+    content:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint excepturi ea explicabo. Illum suscipit illo, porro quisquam voluptatem",
+    imageURL:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faie.edu.au%2Fwp-content%2Fuploads%2F2018%2F03%2Fgame-programming-03.jpg",
+  },
+];
